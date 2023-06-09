56585f34b73243f5bd9f472c58f21ffe3a608ad0
Imran <imranullah114@gmail.com>
2023-06-09 12:10:35 +0500

    changes made

--- src/main/java/org/imran/java/LinkedInAutomation.java
+++ src/main/java/org/imran/java/LinkedInAutomation.java
@@ -17,44 +17,44 @@ public class LinkedInAutomation {
         WebElement password = null;
         // Set the path to chromedriver executable
         System.setProperty("webdriver.chrome.driver", "D:/Gitlab practice/test-project/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(); // Create a new ChromeDriver instance
+        // In case you are using firefox browser, use below commented code and uncomment it
 //        System.setProperty("webdriver.gecko.driver", "D:/Gitlab practice/test-project/geckodriver.exe");
-//        WebDriver driver = new FirefoxDriver();
+//        WebDriver driver = new FirefoxDriver(); // Create a new FirefoxDriver instance
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         // Launch LinkedIn website
         driver.get("https://www.linkedin.com");
 
         System.out.println("LinkedIn launched....");
- Thread.sleep(3000);
+        Thread.sleep(3000); // Wait for 3 seconds
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        // Login to LinkedIn (replace 'username' and 'password' with actual credentials) // //*[@id="session_key"]
-            email = driver.findElement(By.xpath("//div[@class='flex flex-col']//input[contains(@id, 'email-or-phone')] | //input[contains(@autocomplete, 'username')]"));
-        email.sendKeys("email");
-        password = driver.findElement(By.xpath("//div[@class='flex flex-col']//input[contains(@autocomplete, 'new-password')] | //input[contains(@autocomplete, 'current-password')]")); //*[@id="session_password"]
-        password.sendKeys("password");
+
+        // Login to LinkedIn (replace 'username' and 'password' with actual credentials)
+        email = driver.findElement(By.xpath("//div[@class='flex flex-col']//input[contains(@id, 'email-or-phone')] | //input[contains(@autocomplete, 'username')]"));
+        email.sendKeys("email"); // Enter the email
+        password = driver.findElement(By.xpath("//div[@class='flex flex-col']//input[contains(@autocomplete, 'new-password')] | //input[contains(@autocomplete, 'current-password')]"));
+        password.sendKeys("password"); // Enter the password
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        WebElement signIn = driver.findElement(By.cssSelector("button[type='submit']")); //   //button[starts-with(@class, 'btn-md btn-primary flex-shrink-0')]
+        WebElement signIn = driver.findElement(By.cssSelector("button[type='submit']")); // Find the Sign In button
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        signIn.click();
-        driver.manage().window().maximize();
+        signIn.click(); // Click on the Sign In button
+        driver.manage().window().maximize(); // Maximize the browser window
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//        WebElement verifyButton = driver.findElement(By.xpath("//button[@id='home_children_button']"));
-//        if (verifyButton.isDisplayed()) {
-//            verifyButton.click();
-//        } else {
+
         // Search for a company
-        Thread.sleep(3000);
+        Thread.sleep(3000); // Wait for 3 seconds
         WebElement searchInput = driver.findElement(By.cssSelector("input[role='combobox']"));
-        searchInput.sendKeys("Techlogix");
-        searchInput.sendKeys(Keys.RETURN);
+        searchInput.sendKeys("Techlogix"); // Enter the company name
+        searchInput.sendKeys(Keys.RETURN); // Press Enter
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         System.out.println("Company name entered");
+
         // View page
-        Thread.sleep(1000);
+        Thread.sleep(1000); // Wait for 1 second
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         WebElement viewPageButton = driver.findElement(By.xpath("//span[@class='image-text-lockup__text ']"));
-        viewPageButton.click();
+        viewPageButton.click(); // Click on the View Page button
         System.out.println("View Page clicked...");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
@@ -62,25 +62,22 @@ public class LinkedInAutomation {
         List<WebElement> navigationItems = driver.findElements(By.cssSelector(".org-page-navigation__item"));
         for (WebElement item : navigationItems) {
             WebElement anchorElement = item.findElement(By.cssSelector(".org-page-navigation__item-anchor"));
-            if (anchorElement.getText().equals("Posts")) {
+            if (anchorElement.getText().equals("Posts")) { // Check if the navigation item is the "Posts" tab
                 postsTab = anchorElement;
                 break;
             }
         }
         if (postsTab != null) {
-            postsTab.click();
+            postsTab.click(); // Click on the "Posts" tab
             System.out.println("Posts tab clicked...");
         } else {
             System.out.println("Posts tab not found.");
         }
 
-        // ...
-
         boolean postFound;
         List<WebElement> postElements = driver.findElements(By.xpath("//div[contains(@class, 'scaffold-finite-scroll__content')]"));
         int previousSize = 0;
 
-// ...
 
         postFound = false;
 
@@ -92,17 +89,16 @@ public class LinkedInAutomation {
                 for (WebElement spanElement : spanElements) {
                     String postText = spanElement.getText();
 
-                    // ...
 
                     if (postText.contains("java") || postText.contains("Java") || postText.contains("JAVA")  ) {
                         System.out.println("Matching post found. . . . ");
 
-                        // Save drop down click    button.feed-shared-control-menu__trigger
+                        // Save drop down click button.feed-shared-control-menu__trigger
                         List<WebElement> buttons = driver.findElements(By.xpath("//*[starts-with(@id, 'ember') and starts-with(@class, 'feed-shared-control-menu__trigger')]"));
 
                         if (buttons.size() >= 1) {
                             WebElement saveDropDown = buttons.get(0);
-                            saveDropDown.click();
+                            saveDropDown.click(); // Click on the Save dropdown button
                             System.out.println("Button clicked");
 
                             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
@@ -113,9 +109,9 @@ public class LinkedInAutomation {
                             WebElement saveButton = null;
 
                             for (WebElement item : dropdownItems) {
-                                if (item.getText().equals("Save") || item.getText() == "Save") {
+                                if (item.getText().equals("Save") || item.getText() == "Save") { // Check if the item is the "Save" button
                                     saveButton = item;
-                                    saveButton.click();
+                                    saveButton.click(); // Click on the "Save" button
                                     System.out.println("Save Button clicked");
                                     break;
                                 }
@@ -133,15 +129,11 @@ public class LinkedInAutomation {
 
                 if (!postFound) {
                     Actions actions = new Actions(driver);
-                    actions.sendKeys(Keys.PAGE_DOWN).perform();
-                    Thread.sleep(1000);
+                    actions.sendKeys(Keys.PAGE_DOWN).perform(); // Scroll down
+                    Thread.sleep(1000); // Wait for 1 second
                     postElements = driver.findElements(By.xpath("//div[contains(@class, 'scaffold-finite-scroll__content')]"));
                 }
             }
-
-
         }
-
-
     }
 }
